39c5b74cb0f43ea63f7f3b48c9763d53c5472fb6
kumarsachinguri <kumarsachinguri@gmail.com>
2024-06-18 17:00:06 +0530

    update sidebar with working links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
-    path: '', component: HomeComponent, title: 'Home'
-  }
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'snippets', component: HomeComponent },
+  { path: 'blogs', component: HomeComponent },
 ];
