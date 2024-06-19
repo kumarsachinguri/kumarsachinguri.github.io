35e8a3078a82f8ac50144be49df30d2d913aa0ae
kumarsachinguri <kumarsachinguri@gmail.com>
2024-06-19 15:15:35 +0530

    test 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { SOCIAL_MEDIA_LINKS } from '../../shared/constant/social-media-links.constant';
 import { SvgHighlightDirective } from '../../shared/directive/svg-highlight.directive';
-import {
-  KNOWLEDGE_BADGES,
-  KnowledgeBadge,
-} from '../../shared/constant/knowledge-badges.constant';
-import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgStyle, SvgHighlightDirective],
+  imports: [SvgHighlightDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   public socialMediaLinks = SOCIAL_MEDIA_LINKS;
-  public knowledgeBadges: KnowledgeBadge[] = KNOWLEDGE_BADGES;
 }
