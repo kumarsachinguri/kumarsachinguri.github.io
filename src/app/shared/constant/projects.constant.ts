7f19af46323dbb65018b3769a11e3e5210b4864f
kumarsachinguri <kumarsachinguri@gmail.com>
2024-07-21 16:52:28 +0530

    Add Project constants

---
+++ src/app/shared/constant/projects.constant.ts
@@ -0,0 +1,22 @@
+import { Project } from '../interfaces/project.model';
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Amazon Clone',
+    image: 'amazon-clone.jpeg',
+    heading: 'Amazon Clone',
+    skills: ['Angular', 'Typescript', '.Net Core'],
+  },
+  {
+    name: 'Discord Clone',
+    image: 'discord-clone.jpeg',
+    heading: 'Discord Clone',
+    skills: ['Angular', 'Typescript', '.Net Core'],
+  },
+  {
+    name: 'Spotify Clone',
+    image: 'spotify-clone.gif',
+    heading: 'Spotify Clone',
+    skills: ['Angular', 'Typescript', '.Net Core'],
+  },
+];
---
+++ src/app/shared/interfaces/project.model.ts
@@ -0,0 +1,6 @@
+export interface Project {
+  name: string;
+  image: string;
+  heading: string;
+  skills: Array<string>;
+}
